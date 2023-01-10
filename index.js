909759f6b067ac710eac36c145ba012599a0d527
Ezequiel Brilhante <ezequielrb7@gmail.com>
2023-01-10 18:20:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-// get filter element
-// get cards elements
+const filterElement = document.querySelector('header input')
+const cards = document.querySelectorAll('.cards li')
 
-// add input event for the filter element
+filter.addEventListener('input', filterCards)
 
-// filter function
-  // if the filter is not empty
-    // for each card of cards
-      // get card heading (title)
-      // tranform to lower case
-      // transform filter text to lower case
-      // if card title does not include the filter text
-        // hide the card element
-      // else
-        // unhide the card element
-  // else
-    // for each card of cards
-      // unhide the card element
+function filterCards() {
+
+  if (filterElement.value !== '') {
+    for (let card of cards) {
+      let title = card.querySelector('h2')
+      title = title.textContent.toLowerCase()
+      let filterText = filterElement.value.toLowerCase()
+      if (!title.includes(filterText)) {
+        card.style.display = "none"
+      } else {
+        card.style.display = "block"
+      }
+    }
+  } else {
+    for (let card of cards) {
+      card.style.display = "block"
+    }
+  }
+}
